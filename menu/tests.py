b1c636d754604646e4c0ae1dc86caef519b8f3bf
khodjiyev2o <92217667+khodjiyev2o@users.noreply.github.com>
2022-06-25 22:22:25 +0900

    yml file

--- menu/tests.py
+++ menu/tests.py
@@ -1,6 +1,5 @@
 from django.test import TestCase
-
-
+from .models import Product
 from django.contrib.auth.models import User
 # Create your tests here.
 
@@ -9,9 +8,12 @@ class CustomerTestCase(TestCase):
     def setUp(self):
 
         user =  User.objects.create_user('Chevy Chase', 'chevy@chase.com', 'chevyspassword')
-
+        product = Product.objects.create(name="book",price=15,digital=False)
 
 
     def test_contact_count(self):
         self.assertEqual(User.objects.count(),1)
 
+
+    def test_product_counter(self):
+        self.assertEqual(Product.objects.count(),1)
